7e87afebc9eb0622922d58812d7aeac776773aec
Ivan Iglesias <iiglesiasbilbao@gmail.com>
2022-06-01 20:21:26 +0200

    restaurant details router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardRestaurantComponent } from './card-restaurant/card-restaurant.component';
 import { MaterialModule } from '../material.module';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardRestaurantComponent,
-    RestaurantListComponent
+    RestaurantListComponent,
+    RestaurantDetailComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -21,7 +23,8 @@ import { RestaurantListComponent } from './restaurant-list/restaurant-list.compo
     AppRoutingModule,
     MaterialModule,
     RouterModule.forRoot([
-        { path: '', component: RestaurantListComponent }
+        { path: '', component: RestaurantListComponent },
+        { path: 'restaurants/:restaurantId', component: RestaurantDetailComponent },
       ])
   ],
   providers: [],
